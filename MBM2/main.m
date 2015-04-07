55b2a1441a176b5bee8baf34eba13866029e2405
Hemant Torsekar <hvt@me.com>
2015-04-07 10:29:35 -0400

    Initial Commit

---
+++ MBM2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MBM2
+//
+//  Created by Hemant V. Torsekar on 2015-04-07.
+//  Copyright (c) 2015 Hemant V. Torsekar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
